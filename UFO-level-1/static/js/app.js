5b9307b95a912ed644ef00dd2c7b06e56125ce07
danahzhang <55262705+danahzhang@users.noreply.github.com>
2020-04-17 15:46:10 -0700

    Add files via upload

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,38 @@
+// from data.js
+var tableData = data;
+//Create a Table with all the Data
+var tbody = d3.select("tbody");
+tableData.forEach((data) => {
+  var row = tbody.append("tr");
+  Object.entries(data).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+});
+});
+// YOUR CODE HERE!
+//Button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+  //Remove Previous Table
+  tbody.html("");
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+
+  //Filtered Data
+  var filteredData = tableData.filter(data => data.datetime === inputValue);
+
+
+  //Add Filtered Table
+  filteredData.forEach((data) => {
+          var row = tbody.append("tr");
+          Object.entries(data).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+      });
+  });
+});
